f51a8d2858ce62466ee8053eeae98e38c90b5fd1
abdsymoh <abdsydev@gmail.com>
2024-02-18 09:32:08 +0200

    resolve day6 problem

---
+++ js/day6/returningstrings.js
@@ -0,0 +1,15 @@
+// Subscribe to my channel :  https://www.youtube.com/@yalladev
+// Follow me on X (Twitter) : https://twitter.com/abdsymoh
+
+// Day 6 Of 100 Days Of JavaScript Problems Solving
+
+// (6) Returning Strings
+
+function greet(name) {
+  let greeting = `Hello, ${name} how are you doing today?`;
+  return greeting;
+}
+
+console.log(greet("Abdullah"))
+
+// "";
