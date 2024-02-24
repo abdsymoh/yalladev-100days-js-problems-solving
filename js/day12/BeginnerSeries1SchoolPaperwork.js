a264b1a0222aa5afd595a7cdcd7629cce969b668
abdsymoh <abdsydev@gmail.com>
2024-02-24 23:22:56 +0200

    solve day12

---
+++ js/day12/BeginnerSeries1SchoolPaperwork.js
@@ -0,0 +1,12 @@
+// Subscribe to my channel :  https://www.youtube.com/@yalladev
+// Follow me on Telegram : https://t.me/yalladev
+
+// Day 12 Of 100 Days Of JavaScript Problems Solving
+
+// (12) Beginner Series #1 School Paperwork
+
+function paperwork(n, m) {
+  return n < 0 || m < 0 ? 0 : n * m;
+}
+
+console.log(paperwork(-5, -5));
