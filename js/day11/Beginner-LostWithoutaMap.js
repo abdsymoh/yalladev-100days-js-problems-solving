824c12c16a9bec0dc4b55c5d6bb7faa5ac844009
abdsymoh <abdsydev@gmail.com>
2024-02-24 23:06:35 +0200

    solve day11

---
+++ js/day11/Beginner-LostWithoutaMap.js
@@ -0,0 +1,13 @@
+// Subscribe to my channel :  https://www.youtube.com/@yalladev
+// Follow me on Telegram : https://t.me/yalladev
+
+// Day 11 Of 100 Days Of JavaScript Problems Solving
+
+// (11) Beginner - Lost Without a Map
+
+function maps(x) {
+  return x.map(m => m * 2)
+}
+
+let myArr = [1, 2, 3];
+console.log(maps(myArr));
