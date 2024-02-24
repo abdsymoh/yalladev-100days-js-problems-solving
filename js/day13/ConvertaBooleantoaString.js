6236d5e15cda73a8cc39cadd8b83136e0a87cdd0
abdsymoh <abdsydev@gmail.com>
2024-02-24 23:31:17 +0200

    solve day13

---
+++ js/day13/ConvertaBooleantoaString.js
@@ -0,0 +1,11 @@
+// Subscribe to my channel :  https://www.youtube.com/@yalladev
+// Follow me on Telegram : https://t.me/yalladev
+
+// Day 13 Of 100 Days Of JavaScript Problems Solving
+
+// (13) Convert a Boolean to a String
+
+function booleanToString(b) {
+  return b.toString();
+}
+console.log(booleanToString(true));
