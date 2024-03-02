7b6e2ae3176e90700e4559d1e5f402b3203b18b6
abdsymoh <abdsydev@gmail.com>
2024-03-02 07:37:00 +0200

    solve day 20

---
+++ js/day20/simplemultiplication.js
@@ -0,0 +1,12 @@
+// Subscribe to my channel :  https://www.youtube.com/@yalladev
+// Follow me on Telegram : https://t.me/yalladev
+
+// Day 20 Of 100 Days Of JavaScript Problems Solving
+
+// (20) Simple multiplication
+
+function simpleMultiplication(number) {
+  return number % 2 === 0 ? number * 8 : number * 9;
+}
+
+console.log(simpleMultiplication(2));
