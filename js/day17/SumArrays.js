c2d971c5f7ea3d9f8767e82c08c55f00ff71f40e
abdsymoh <abdsydev@gmail.com>
2024-03-02 06:21:19 +0200

    solve day 17

---
+++ js/day17/SumArrays.js
@@ -0,0 +1,18 @@
+// Subscribe to my channel :  https://www.youtube.com/@yalladev
+// Follow me on Telegram : https://t.me/yalladev
+
+// Day 17 Of 100 Days Of JavaScript Problems Solving
+
+// (17) Sum Arrays
+
+function sum(numbers) {
+  let sum = 0;
+  for (n of numbers) {
+    sum += n
+  }
+  return sum
+}
+
+// let arr = [1, 5.2, 4, 0, -1];
+// let arr = [];
+console.log(sum(arr));
