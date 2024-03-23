27f937f7d66450a8606a0931153ae6199bcf89f5
abdsymoh <abdsydev@gmail.com>
2024-03-23 14:01:17 +0200

    solve day 32

--- js/day33/sumoftwolowestpositiveintegers.js
+++ js/day33/sumoftwolowestpositiveintegers.js
@@ -7,7 +7,7 @@
 // (33) Sum of two lowest positive integers
 
 function sumTwoSmallestNumbers(numbers) {
-  numbers = numbers.sort((a, b) => a - b);
+  numbers = numbers.sort((a,b) => a - b)
   return numbers[0] + numbers[1];
 }
 
