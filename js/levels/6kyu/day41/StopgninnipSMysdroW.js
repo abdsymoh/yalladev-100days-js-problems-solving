02b39802ab31ea4079096c639986936c9b1d3968
abdsymoh <abdsydev@gmail.com>
2024-03-31 00:02:08 +0200

    solve day 40 & prepare day 41

---
+++ js/levels/6kyu/day41/StopgninnipSMysdroW.js
@@ -0,0 +1,23 @@
+// Subscribe to my channel :  https://www.youtube.com/@yalladev
+// Follow me on Telegram : https://t.me/abdsymoh
+
+// Problems Solving With JavaScript # 40
+//  Problem Level : [ 6 kyu ]
+// (41) Stop gninnipS My sdroW!
+
+// if word have five or more letters reversed
+
+function spinWords(string) {
+  return string;
+}
+
+console.log(spinWords("Welcome"));
+// "emocleW"
+console.log(spinWords("Hey fellow warriors"));
+// "Hey wollef sroirraw"
+console.log(spinWords("This is a test"));
+// "This is a test"
+console.log(spinWords("This is another test"));
+//"This is rehtona test"
+console.log(spinWords("Just kidding there is still one more"));
+// "Just gniddik ereht is llits one more"
---
+++ js/levels/7kyu/day40/BinaryAddition.js
@@ -0,0 +1,14 @@
+// Subscribe to my channel :  https://www.youtube.com/@yalladev
+// Follow me on Telegram : https://t.me/abdsymoh
+
+// Problems Solving With JavaScript # 40
+//  Problem Level : [ 7 kyu ]
+// (40) Binary Addition
+
+function addBinary(a, b) {
+  return (a + b).toString(2)
+}
+
+console.log(addBinary(1, 1)); // "10"
+console.log(addBinary(1, 2)); // "11"
+console.log(addBinary(5, 9)); // "1110"
