ce18427c06793c32822dcfcbb5004a9d71a66fab
abdsymoh <abdsydev@gmail.com>
2024-04-06 06:10:15 +0200

    prepare day46

---
+++ js/levels/6kyu/day46/BitCounting.js
@@ -0,0 +1,10 @@
+// Subscribe to my channel :  https://www.youtube.com/@yalladev
+// Follow me on Telegram : https://t.me/abdsymoh
+
+// Problems Solving With JavaScript # (46) Create Phone Number
+//  Problem Level : [ 6 kyu ]
+// Problem Link : https://www.codewars.com/kata/526571aae218b8ee490006f4
+
+var countBits = function (n) {
+  // Program Me
+};
