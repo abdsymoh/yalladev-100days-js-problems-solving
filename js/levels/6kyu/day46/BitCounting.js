2e9d81a8b91aeec867d986260cf009a4ab010b0f
abdsymoh <abdsydev@gmail.com>
2024-04-06 16:40:18 +0200

    solve day 46

--- js/levels/6kyu/day46/BitCounting.js
+++ js/levels/6kyu/day46/BitCounting.js
@@ -6,5 +6,11 @@
 // Problem Link : https://www.codewars.com/kata/526571aae218b8ee490006f4
 
 var countBits = function (n) {
-  // Program Me
+  return n.toString(2).match(/1/g) === null ? 0 : n.toString(2).match(/1/g).length;
 };
+
+console.log(countBits(0)); // 0
+console.log(countBits(4)); // 1
+console.log(countBits(7)); // 3
+console.log(countBits(9)); // 2
+console.log(countBits(10)); // 2
