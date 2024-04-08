f0d40437d3891da30a2f7d556c4f66bc2194cd37
abdsymoh <abdsydev@gmail.com>
2024-04-08 21:55:46 +0200

    prepare day49

---
+++ js/levels/6kyu/day49/ReplaceWithAlphabetPosition.js
@@ -0,0 +1,14 @@
+// Subscribe to my channel :  https://www.youtube.com/@yalladev
+// Follow me on Telegram : https://t.me/abdsymoh
+
+// Problems Solving With JavaScript # (49) Replace With Alphabet Position
+//  Problem Level : [ 6 kyu ]
+// Problem Link : https://www.codewars.com/kata/546f922b54af40e1e90001da
+
+function alphabetPosition(text) {
+  const alphabet = "abcdefghijklmnopqrstuvwxyz".split("");
+  return text;
+}
+
+console.log(alphabetPosition("The sunset sets at twelve o' clock."));
+console.log(alphabetPosition("The narwhal bacons at midnight."));
