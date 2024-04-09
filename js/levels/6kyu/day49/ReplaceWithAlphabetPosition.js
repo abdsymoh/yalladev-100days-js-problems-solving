d3896120a60fa74ea29aa3d7827e084a7ff3d8e5
abdsymoh <abdsydev@gmail.com>
2024-04-09 15:48:25 +0200

    solve day49

--- js/levels/6kyu/day49/ReplaceWithAlphabetPosition.js
+++ js/levels/6kyu/day49/ReplaceWithAlphabetPosition.js
@@ -5,10 +5,24 @@
 //  Problem Level : [ 6 kyu ]
 // Problem Link : https://www.codewars.com/kata/546f922b54af40e1e90001da
 
+/*
+given a string, replace every letter with its position in the alphabet.
+If anything in the text isn't a letter, ignore it and don't return it.
+"a" = 1, "b" = 2, etc.
+*/
+
 function alphabetPosition(text) {
-  const alphabet = "abcdefghijklmnopqrstuvwxyz".split("");
-  return text;
+  let alphabet = "abcdefghijklmnopqrstuvwxyz";
+  return text
+    .toLowerCase()
+    .split("")
+    .filter((c) => c.match(/[a-z]/i))
+    .map((c, i, a) => c.replace(c, alphabet.indexOf(c) + 1))
+    .join(" ");
 }
 
 console.log(alphabetPosition("The sunset sets at twelve o' clock."));
+// "20 8 5 19 21 14 19 5 20 19 5 20 19 1 20 20 23 5 12 22 5 15 3 12 15 3 11"
+
 console.log(alphabetPosition("The narwhal bacons at midnight."));
+// "20 8 5 14 1 18 23 8 1 12 2 1 3 15 14 19 1 20 13 9 4 14 9 7 8 20"
