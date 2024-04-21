b9bc7a282f24caf5347de4c08d893fcddd8ecfda
abdsymoh <abdsydev@gmail.com>
2024-04-21 10:25:50 +0200

    solve day 54

--- js/levels/6kyu/day54/SplitStrings.js
+++ js/levels/6kyu/day54/SplitStrings.js
@@ -5,3 +5,19 @@
 // (54) Split Strings
 // https://www.codewars.com/kata/515de9ae9dcfc28eb6000001
 
+function solution(str) {
+  let paris = [];
+  for (i = 0; i < str.length; i += 2) {
+    paris.push(str.substring(i, i + 2));
+  }
+  for (p in paris) {
+    if (paris[p].length === 1) {
+      paris[p] = paris[p] + "_";
+    }
+  }
+  return paris;
+}
+
+console.log(solution("abcdef")); // ["ab", "cd", "ef"]
+console.log(solution("abcdefg")); // ["ab", "cd", "ef", "g_"]
+console.log(solution("")); // []
